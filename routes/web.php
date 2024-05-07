9271bf67eb5bff568e3bca9c9230ce3f881184b7
NaveenJoshuvaDev <96105253+NaveenJoshuvaDev@users.noreply.github.com>
2024-05-07 22:52:47 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -47,5 +47,19 @@ Route::get('/users/{userId}/expenses', [UserController::class, 'showExpenses'])-
 
 Route::get('/users/index', [UserController::class, 'index'])->name('users.index');//wrong
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('users.dashboard');
+    Route::get('/expenseindex', [UserController::class, 'welcome'])->name('users.expense.index');
+    Route::post('/users/{userId}/expenses', [UserController::class, 'store'])->name('users.expenses.store');
+    Route::put('/expenses/{id}', [UserController::class, 'update'])->name('expenses.update');
+    Route::delete('/users/expense/{userId}', [UserController::class, 'destroy'])->name('users.expense.destroy');
+    Route::get('/editpage/{userId}', [UserController::class, 'edit'])->name('users.expense.edit');
+
+    // This route seems incorrect, assuming it's meant to show expenses for a specific user
+    Route::get('/users/{userId}/expenses', [UserController::class, 'showExpenses'])->name('users.expenses.index');
+
+    // This route also seems incorrect, assuming it's meant to show the index page for users
+    Route::get('/users/index', [UserController::class, 'index'])->name('users.index');
+});
 
 
